934955e209072491b1e2736be9f2683d4086ddac
Sujin <sjnnus@gmail.com>
2022-03-01 17:39:47 +0900

    1st commit
    1st commit

---
+++ bttf_project/WebContent/resource/js/board.js
@@ -0,0 +1,10 @@
+jQuery(function($){
+   $("#foo-table").DataTable({
+      "bInfo": false,
+      "bSortable" : false,
+      "bPaginate" : true,
+      "displayLength" : 5
+      //      "ajax": 'boardJson.json'   
+   });
+   
+});
